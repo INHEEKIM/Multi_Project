3fd370cf390138016c91add79bad6301cc54c4b4
INHEEKIM <tie_ara@naver.com>
2017-01-31 13:56:47 +0900

    HelloWorld 스크립트 추가/ 기능은 Cube 오브젝트 디버그 로그로 이름 찍기

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+    GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+        cube = GameObject.Find("Cube");
+        Debug.Log(cube.gameObject.name);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
