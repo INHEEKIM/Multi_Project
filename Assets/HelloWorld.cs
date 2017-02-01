c0cbe15f423067a32417645ae70d0834511da7a8
kmjae05 <kangna58@naver.com>
2017-02-01 17:37:40 +0900

    코루틴 또 추가

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -4,21 +4,26 @@ using System.Collections;
 public class HelloWorld : MonoBehaviour {
 
     GameObject cube;
+    IEnumerator ienumerator;
 
 	// Use this for initialization
 	void Start () {
         Debug.Log("Hi");
+
+        ienumerator = click();
         //StartCoroutine("click");
-        StartCoroutine(this.click());
+        //StartCoroutine(this.click());
+        StartCoroutine(ienumerator);
        
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-	
+        //StopCoroutine(ienumerator); //멈출 때 이런 식으로.
 	}
 
+    //오브젝트가 비활성되면 자동으로 소멸.
     IEnumerator click()
     {
         Debug.Log("before");
